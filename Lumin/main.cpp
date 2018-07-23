03f52a6ad3da747acbb82e2b0f7072b9875eed61
Omar M <LumiumEngine@gmail.com>
2018-07-23 14:18:49 -0600

    Fixed debugging on openGL

--- Lumin/main.cpp
+++ Lumin/main.cpp
@@ -1,29 +1,49 @@
+#include <LumiumEngine/Graphics/Vertex.hpp>
 #include <LumiumEngine/System/Window.hpp>
 #include <LumiumEngine/System/InputManager.hpp>
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
+#include <iostream>
+#include <vector>
 
 int main(int argc, char **argv)
 {
-	lumi::Window window;
+	lumi::system::Window window;
 	if (window.createWindow("Hello World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL))
 	{
 		gladLoadGLLoader(SDL_GL_GetProcAddress);
 
+		std::vector<lumi::graphics::Vertex> vertices;
+
 		glClearColor(.2f, .4f, .6f, 1.0f);
-		GLfloat verticies[] = { -1, -1,  1,
-			0,  1,  1,
-			1, -1,  1 };
+		glm::vec3 verticies[] = 
+		{
+			glm::vec3(-1, -1,  1),
+			glm::vec3( 0,  1,  1),
+			glm::vec3( 1, -1,  1) 
+		};
+		for (int i = 0; i < 3; i++)
+		{
+			lumi::graphics::Vertex vert;
+			vert.Position = verticies[i];
+			vertices.push_back(vert);
+		}
 		GLuint vao, vbo;
 		glCreateVertexArrays(1, &vao); // vertex array object
+
 		glCreateBuffers(1, &vbo);	// vertex buffer
-		glNamedBufferData(vbo, sizeof(verticies), verticies, GL_STATIC_DRAW);
+		glNamedBufferStorage(vbo, vertices.size() * sizeof(lumi::graphics::Vertex), vertices.data(), GL_DYNAMIC_STORAGE_BIT);
+		
+		glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(lumi::graphics::Vertex));
+
 		glEnableVertexArrayAttrib(vao, 0); // vao, index
-		glVertexArrayVertexBuffer(vao, 0, vbo, 0, 3 * sizeof(GLfloat));
-		glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+
+		glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, offsetof(lumi::graphics::Vertex, Position));
+		
 		glVertexArrayAttribBinding(vao, 0, 0);
 
-		lumi::InputManager iManager;
+		lumi::system::InputManager iManager;
 		SDL_Event event;
 		while (window.isOpen())
 		{
