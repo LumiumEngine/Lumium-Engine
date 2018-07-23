ba2f35913b4d3544db98a6d2bc4e6de52608062d
Omar M <LumiumEngine@gmail.com>
2018-07-23 16:37:18 -0600

    Updated example to include our Buffer class

--- Lumin/main.cpp
+++ Lumin/main.cpp
@@ -1,8 +1,9 @@
-#include <LumiumEngine/Graphics/Vertex.hpp>
-#include <LumiumEngine/System/Window.hpp>
-#include <LumiumEngine/System/InputManager.hpp>
 #include <glad/glad.h>
 #include <glm/glm.hpp>
+#include <LumiumEngine/Graphics/Buffers.hpp>
+#include <LumiumEngine/Graphics/Vertex.hpp>
+#include <LumiumEngine/System/InputManager.hpp>
+#include <LumiumEngine/System/Window.hpp>
 
 #include <iostream>
 #include <vector>
@@ -17,31 +18,22 @@ int main(int argc, char **argv)
 		std::vector<lumi::graphics::Vertex> vertices;
 
 		glClearColor(.2f, .4f, .6f, 1.0f);
+
 		glm::vec3 verticies[] = 
 		{
+			glm::vec3(-1,  1,  1),
+			glm::vec3( 1,  1,  1),
 			glm::vec3(-1, -1,  1),
-			glm::vec3( 0,  1,  1),
-			glm::vec3( 1, -1,  1) 
+			glm::vec3(-1,  1,  1)
 		};
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < 4; i++)
 		{
 			lumi::graphics::Vertex vert;
 			vert.Position = verticies[i];
 			vertices.push_back(vert);
 		}
-		GLuint vao, vbo;
-		glCreateVertexArrays(1, &vao); // vertex array object
-
-		glCreateBuffers(1, &vbo);	// vertex buffer
-		glNamedBufferStorage(vbo, vertices.size() * sizeof(lumi::graphics::Vertex), vertices.data(), GL_DYNAMIC_STORAGE_BIT);
-		
-		glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(lumi::graphics::Vertex));
-
-		glEnableVertexArrayAttrib(vao, 0); // vao, index
-
-		glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, offsetof(lumi::graphics::Vertex, Position));
-		
-		glVertexArrayAttribBinding(vao, 0, 0);
+		lumi::graphics::Buffers buffer;
+		buffer.createBuffers(vertices);
 
 		lumi::system::InputManager iManager;
 		SDL_Event event;
@@ -53,9 +45,9 @@ int main(int argc, char **argv)
 					window.closeWindow();
 			}
 			glClear(GL_COLOR_BUFFER_BIT);
-			glBindVertexArray(vao);
+			buffer.bindBuffers();
 			glDrawArrays(GL_TRIANGLES, 0, 3);
-			glBindVertexArray(0);
+			buffer.unbindBuffers();
 			window.display();
 		}
 	}
